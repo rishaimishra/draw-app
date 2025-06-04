8941974f786c4a58e33ce14364abd0084ee2f4de
rishavbeas <rishavbeas@gmail.com>
2025-06-04 22:25:57 +0530

    added middleware jwt token and routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,50 @@
-import express from "express"
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
+app.use(express.json());
 
-app.listen(3001)
+app.post("/signup", (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    res.send({
+        message: "You have signed up",
+    });
+});
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = 1;
+
+    if (user) {
+        const token = jwt.sign(
+            {
+                username: user,
+            },
+            JWT_SECRET
+        );
+
+        res.send({
+            token,
+        });
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password",
+        });
+    }
+});
+
+app.get("/room", middleware, (req, res) => {
+    const token = req.headers.authorization;
+    res.status(403).send({
+        message: "Invalid username or password",
+    });
+});
+
+app.listen(3001);
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+interface User {
+	userId: string;
+}
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers["authorization"] ?? "";
+    const decoded = jwt.verify(token, JWT_SECRET) as User;
+
+    if (decoded) {
+        (req as any).userId = decoded.userId;
+    } else {
+        res.status(403).json({
+            message: "UnAuthorized"
+        })
+    }
+
+}
